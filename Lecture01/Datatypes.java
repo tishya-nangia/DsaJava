f4732df0776c5f3a87afaa06cc8481c1c6f77b81
tishya-nangia <nangiatishya@gmail.com>
2024-09-08 15:25:48 +0530

    Second Commit

---
+++ Lecture01/Datatypes.java
@@ -0,0 +1,10 @@
+package Lecture01;
+
+public class Datatypes {
+    public static void main(String[] args) {
+        //Primitive Data types
+        int a=20;
+        System.out.println(a);
+        System.out.print(a);
+    }
+}
